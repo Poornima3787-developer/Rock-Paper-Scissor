0ca2c9bce5676e1b5ccc45e8bec919138e170640
POORNIMA <taehyungpurple02@gmail.com>
2024-11-16 13:38:12 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String[] str={"Rock","Paper","Scissor"};
+        Scanner sc=new Scanner(System.in);
+        sc.nextInt();
+        sc.nextLine();
+        while(){
+            if()
+        }
         System.out.println("Hello world!");
     }
 }
